4786f6a3fd8086f412e403f991bfdd24203330f3
moliveros <mcoliveros1202@gmail.com>
2022-08-28 10:12:47 -0500

    added getter to thoughts

--- models/Thought.js
+++ models/Thought.js
@@ -4,26 +4,33 @@ const dateFormat = require('../utils/dataFormat');
 
 // Thought model contains:
 const ThoughtSchema = new Schema({
-// thoughtText: string, required, be between 1 and 280 characters
+    // thoughtText: string, required, be between 1 and 280 characters
     thoughtText: {
         type: String,
     },
 
-// createdAt: date, set default value to current timestamp, use getter method to format the timestamp on query
+    // createdAt: date, set default value to current timestamp, use getter method to format the timestamp on query
     createdAt: {
         type: Date,
         default: Date.now,
         get: (createdAtVal) => dateFormat(createdAtVal)
     },
 
-// username: string, required
+    // username: string, required
     username: {
         type: String
     },
-    
-// reactions: [array of nested documents created with the reactionSchema]
-//     reactions: []
-});
+    // reactions: [array of nested documents created with the reactionSchema]
+    //     reactions: []
+},
+    {
+        toJSON: {
+            virtuals: true,
+            getters: true
+        },
+        id: false
+    }
+);
 
 const Thought = model('Thought', ThoughtSchema);
 
--- models/User.js
+++ models/User.js
@@ -24,6 +24,7 @@ const UserSchema = new Schema({
     {
         toJSON: {
             virtuals: true,
+            getters: true
         },
         id: false
     }
